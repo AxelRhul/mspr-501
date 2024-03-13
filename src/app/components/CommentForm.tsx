8a8872a7ba5a6415b7292347d7293ddd1425e677
Axel Rhul <axel.rhul@gmail.com>
2024-03-13 22:54:57 +0100

    fix: rename && clean code

--- src/app/components/CommentForm.tsx
+++ src/app/components/CommentForm.tsx
@@ -1,39 +1,11 @@
-import React, { useEffect, useState } from "react";
-import {useSession} from "next-auth/react";
+"use client";
+import {SessionProvider} from "next-auth/react";
+import CommentFormServer from "@/app/components/CommentFormServer";
 
-export default function CommentForm({plantId}) {
-    const { data: session, status } = useSession()
-    let userEmail;
-
-    if (status === "loading") {
-        return <div>Loading...</div>; // or some other loading indicator
-    }
-
-    if (session) {
-        userEmail = session.user.email;
-    }
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-
-        formData.append('plant-id', plantId);
-        formData.append('user-email', userEmail);
-
-        const fetchResponse = await fetch('/api/comments',
-            {
-                method: 'POST',
-                body: formData,
-            })
-
-        window.location.reload();
-    }
-
+export default function CommentForm({session, plantId}) {
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="content">Commentaire</label>
-            <textarea id="content" name="content" required/>
-            <button type="submit">Envoyer</button>
-        </form>
+       <SessionProvider session={session}>
+           <CommentFormServer plantId={plantId}></CommentFormServer>
+       </SessionProvider>
     )
 }
--- src/app/plantes/[id]/page.tsx
+++ src/app/plantes/[id]/page.tsx
@@ -1,8 +1,8 @@
 import React, {FormEvent, useEffect, useState} from "react";
 import {DisplayPlant} from "@/app/components/DisplayPlant";
-import CommentForm from "@/app/components/CommentForm";
+import CommentFormServer from "@/app/components/CommentFormServer";
 import {SessionProvider} from "next-auth/react";
-import Form from "@/app/components/form";
+import CommentForm from "@/app/components/CommentForm";
 
 export default async function ShowPlants({params}: { params: { id: string } },) {
     const plant = await fetch(`${process.env.BASE_URL}/api/plants/${params.id}`)
@@ -11,19 +11,30 @@ export default async function ShowPlants({params}: { params: { id: string } },) 
     const comments = await fetch(`${process.env.BASE_URL}/api/comments/${params.id}`)
         .then(response => response.json())
 
-    console.log(comments)
     return (
         <>
             <DisplayPlant plant={plant}/>
             <h3>Commentaires</h3>
-            {comments.map((comment) => (
-                <>
-                    <p>Posté par {comment.user.name}, le {comment.createdAt}</p>
-                    <p key={comment.id}>{comment.content}</p>
+            {comments.map((comment) => {
+                const date = new Date(comment.createdAt);
+                const day = String(date.getDate()).padStart(2, '0');
+                const month = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
+                const year = date.getFullYear();
+                const hours = String(date.getHours()).padStart(2, '0');
+                const minutes = String(date.getMinutes()).padStart(2, '0');
 
-                </>
-            ))}
-            <Form plantId={plant.id}/>
+                const formattedDate = `${day}/${month}/${year} à ${hours}:${minutes}`;
+
+                return(
+                    <>
+                        <p>Posté par {comment.user.name}, le {formattedDate}</p>
+                        <p key={comment.id}>{comment.content}</p>
+
+                    </>
+                )
+            }
+            )}
+            <CommentForm plantId={plant.id}/>
         </>
     )
 }
