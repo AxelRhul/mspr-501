1e7694115c2e7e2d3509538983a227292cc60725
Mielot Richard <mielotr@gmail.com>
2024-03-14 23:18:36 +0100

    First commit - Added Header, Map, Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,32 @@ export default function Home() {
             .then(data => setPlants(data));
     }, []);
 
+    console.log(plants);
     return (
         <>
-            <Header/>
-            <div>
-                {plants.map((plant) => (
-                    <a key={plant.id} href={`/plantes/${plant.id}`}>
-                        <h2>{plant.name}</h2>
-                        <p>Plante de {plant.userName}</p>
-                        {plant.images.map((image) => (
-                            <img key={image.id} src={image.url} alt={plant.name} />
-                        ))}
-                    </a>
-                ))}
-                <a href={"/plantes/ajouter"}>Ajouter une plante</a>
+            <Header />
+            <div className="mx-6">
+                <div className="flex flex-row items-center justify-end mb-4 space-x-20">
+                    <h1 className="text-lg text-[#80CC28] font-bold flex items-center justify-center">Nouvelles Plantes</h1>
+                    <a className="border-2 border-[#80CC28] py-0.5 px-1 rounded-lg" href={"/plantes/ajouter"}>Ajouter une plante</a>
+                </div>
+                <div className="grid grid-cols-3 gap-y-4">
+                    {plants.map((plant) => (
+                        <div className="flex flex-col items-center justify-center space-y-2">
+                            <div className="flex flex-row items-center space-x-8">
+                                <h2 className="text-sm underline underline-offset-2 decoration-[#80CC28]">{plant.name}</h2>
+                                <p className="text-sm">{plant.userName}</p>
+                            </div>
+                            <div className="rounded-lg">
+                                <a key={plant.id} href={`/plantes/${plant.id}`} className="">
+                                    {plant.images.map((image) => (
+                                        <img className="object-cover rounded-lg w-48 h-44" key={image.id} src={image.url} alt={plant.name} />
+                                    ))}
+                                </a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
--- src/app/plantes/ajouter/page.tsx
+++ src/app/plantes/ajouter/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Header from '@/components/header';
 import { FormEvent, useRef } from 'react'
 import React, { useState } from "react";
 import Webcam from "react-webcam";
@@ -17,7 +18,7 @@ export default function Page() {
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
 
-        if(photo !== "") {
+        if (photo !== "") {
             const response = await fetch(photo);
             const blob = await response.blob();
             const file = new File([blob], "photo.png", { type: "image/png" });
@@ -30,7 +31,7 @@ export default function Page() {
         const fetchResponse = await fetch('/api/plants',
             {
                 method: 'POST',
-                body:  formData,
+                body: formData,
             })
 
         window.location.reload();
@@ -38,19 +39,30 @@ export default function Page() {
 
     return (
         <>
-        <form onSubmit={onSubmit} encType="multipart/form-data">
-            <input type="text" name="name" placeholder="Votre nom" required={true}/>
-            <input type="text" name="plant-name" placeholder="Nom de la plante" required={true}/>
-            <input type="file" name="images" multiple />
-            <Webcam
-                audio={false}
-                ref={webcamRef}
-                screenshotFormat="image/png"
-            />
-            <button type="submit">Submit</button>
-        </form>
-    <button onClick={capture}>Capture photo</button>
-    {photo && <img src={photo} alt="The taken photo" />}
+            <Header />
+            <form onSubmit={onSubmit} encType="multipart/form-data" className='flex flex-col'>
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/png"
+                    className='mb-4 max-h-screen'
+                />
+                <div className='flex flex-col items-center justify-center'>
+                    <div className='flex items-center justify-between mx-auto my-4 space-x-4'>
+                        <input className='border-2 border-[#80CC28] rounded-lg p-2' type="text" name="name" placeholder="Votre nom" required={true} />
+                        <input className='border-2 border-[#80CC28] rounded-lg p-2' type="text" name="plant-name" placeholder="Nom de la plante" required={true} />
+                    </div>
+                    <button className='group flex flex-row items-center border-2 border-[#80CC28] rounded-lg p-2 mx-auto space-x-3 my-4 hover:bg-[#80CC28] hover:text-[#FCFCFC] hover:p-0' onClick={capture}>
+                        <img className='group-hover:bg-[#FCFCFC] group-hover:p-2 group-hover:rounded-l-md' src="/img/camera.svg" alt="Appareil Photo" />
+                        <span className='group-hover:pr-2'>Prendre une photo</span>
+                    </button>
+                    <div className='flex items-center justify-between mx-auto my-4 space-x-4'>
+                        <input className='w-full' type="file" name="images" multiple />
+                        <button className='bg-[#80CC28] hover:bg-[#6cb821] px-2 py-1 rounded-md text-[#FCFCFC]' type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+            {photo && <img src={photo} alt="The taken photo" />}
         </>
     )
 }
