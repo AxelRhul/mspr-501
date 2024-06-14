b52dbaa4afab51148d70bef324d863e5f57e1cc4
Mielot Richard <mielotr@gmail.com>
2024-06-14 16:14:27 +0200

    Adding modal Historique, modal Gardien

--- src/components/modalHistorique.tsx
+++ src/components/modalHistorique.tsx
@@ -1,7 +1,7 @@
-import { ModalProps } from "@/interface/modalInterface";
+import { ModalHistoriqueProps } from "@/interface/modalHistoriqueInterface";
 import React, { FC } from "react";
 
-const ModalHistorique: FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const ModalHistorique: FC<ModalHistoriqueProps> = ({ isOpen, onClose, plant, children }) => {
   if (!isOpen) return null;
 
   const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -19,6 +19,12 @@ const ModalHistorique: FC<ModalProps> = ({ isOpen, onClose, children }) => {
         <button className="absolute top-6 right-6" onClick={onClose}>
           <img className="w-5" src="/img/fermer.svg" alt="Croix" />
         </button>
+        <h2 className="text-lg xl:text-2xl font-bold mb-4">Historique de <span className="text-vert">{plant.name}</span></h2>
+        <div className="grid grid-cols-2 gap-4">
+          {plant.images.map((image) => (
+            <img className="object-cover rounded-xl w-28 h-28 lg:w-40 lg:h-40 xl:w-52 xl:h-52" key={image.id} src={image.url} alt={plant.name} />
+          ))}
+        </div>
         {children}
       </div>
     </div>
--- src/components/photoTaker.tsx
+++ src/components/photoTaker.tsx
@@ -1,15 +1,15 @@
 "use client"
-import {FormEvent, useEffect, useRef} from 'react'
+import { FormEvent, useEffect, useRef } from 'react'
 import React, { useState } from "react";
 import Webcam from "react-webcam";
 
-export default function PhotoTaker({plantId} : {plantId: string}) {
+export default function PhotoTaker({ plantId }: { plantId: string }) {
     const [photo, setPhoto] = useState("");
 
     const webcamRef = useRef<Webcam | null>(null);
 
     const capture = () => {
-        if(webcamRef.current === null) return;
+        if (webcamRef.current === null) return;
         const imageSrc = webcamRef.current.getScreenshot();
         setPhoto(String(imageSrc));
     };
@@ -21,12 +21,12 @@ export default function PhotoTaker({plantId} : {plantId: string}) {
         if (photo !== "") {
             const response = await fetch(photo);
             const blob = await response.blob();
-            const file = new File([blob], "photo.png", {type: "image/png"});
+            const file = new File([blob], "photo.png", { type: "image/png" });
 
             formData.append('images', file);
         }
 
-        const fetchResponse = await fetch('/api/plants/'+plantId+'/image',
+        const fetchResponse = await fetch('/api/plants/' + plantId + '/image',
             {
                 method: 'POST',
                 body: formData,
@@ -37,17 +37,17 @@ export default function PhotoTaker({plantId} : {plantId: string}) {
 
     return (
         <>
-        <form onSubmit={onSubmit} encType="multipart/form-data">
-                    <input type="file" name="images" multiple/>
-                    <Webcam
-                        audio={false}
-                        ref={webcamRef}
-                        screenshotFormat="image/png"
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-                <button onClick={capture}>Capture photo</button>
-                {photo && <img src={photo} alt="The taken photo"/>}
+            <form onSubmit={onSubmit} encType="multipart/form-data">
+                <input type="file" name="images" multiple />
+                <Webcam
+                    audio={false}
+                    ref={webcamRef}
+                    screenshotFormat="image/png"
+                />
+                <button type="submit">Submit</button>
+            </form>
+            <button onClick={capture}>Capture photo</button>
+            {photo && <img src={photo} alt="The taken photo" />}
         </>
     )
 }
