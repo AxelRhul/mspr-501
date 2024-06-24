9ec1e623b9f5ad0f43d0e6c772d61051b9152f28
Mielot Richard <mielotr@gmail.com>
2024-06-24 08:54:27 +0200

    Update profil plante

--- src/components/modalGardien.tsx
+++ src/components/modalGardien.tsx
@@ -23,22 +23,22 @@ const ModalGardien: FC<ModalGardienProps> = ({ isOpen, onClose, plant, children 
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
       onClick={handleBackgroundClick}
     >
-      <div className="bg-white p-6 rounded-3xl shadow-lg relative w-4/12 mx-auto">
+      <div className="bg-white p-6 rounded-3xl shadow-lg relative w-6/12 xl:w-4/12 mx-auto">
         <button className="absolute top-6 right-6" onClick={onClose}>
           <img className="w-5" src="/img/fermer.svg" alt="Croix" />
         </button>
-        <h2 className="text-lg xl:text-2xl font-bold mb-4">Vous souhaitez garder <span className="text-vert">{plant && plant.name}</span> ?</h2>
-        <p className="text-base font-medium pb-4">Pour ce faire, il vous suffit de vous présenter dans l&apos;encadré ci-dessous.</p>
+        <h2 className="text-base xl:text-2xl font-bold mb-4">Vous souhaitez garder <span className="text-vert">{plant && plant.name}</span> ?</h2>
+        <p className="text-sm lg:text-base font-medium pb-4">Pour ce faire, il vous suffit de vous présenter dans l&apos;encadré ci-dessous.</p>
         <form className="flex flex-col items-center justify-center w-full space-y-4">
           <textarea name="gardiennage" id="gardiennage" className="w-full border-2 border-vert rounded-xl"></textarea>
-          <div className="w-full flex items-center justify-left space-x-4 my-4">
-            <div className="flex flex-col items-left space-y-2">
-              <label className="bloc text-black underline underline-offset-4 decoration-2 decoration-vert">Date de début :</label>
+          <div className="w-full flex flex-col lg:flex-row items-center justify-center lg:justify-between space-y-4 lg:space-y-0 lg:space-x-4 my-4">
+            <div className="w-full flex flex-col items-left space-y-2">
+              <label className="bloc text-sm lg:text-base text-black underline underline-offset-4 decoration-2 decoration-vert">Date de début :</label>
               <DatePicker
                 selected={startDate || undefined}
                 onChange={(date) => setStartDate(date)}
-                className="w-full border-2 border-vert rounded-xl px-2 py-1"
-                placeholderText="Choisissez une date de début"
+                className="w-full border-2 border-vert rounded-xl px-1 py-px lg:px-2 lg:py-1"
+                placeholderText="01/01/1970"
                 locale="fr"
                 dateFormat="dd/MM/yyyy"
                 startDate={startDate || undefined}
@@ -46,13 +46,13 @@ const ModalGardien: FC<ModalGardienProps> = ({ isOpen, onClose, plant, children 
                 selectsStart
               />
             </div>
-            <div className="flex flex-col items-left space-y-2">
-              <label className="bloc text-black underline underline-offset-4 decoration-2 decoration-vert">Date de fin :</label>
+            <div className="w-full flex flex-col items-left space-y-2">
+              <label className="bloc text-sm lg:text-base text-black underline underline-offset-4 decoration-2 decoration-vert">Date de fin :</label>
               <DatePicker
                 selected={endDate || undefined}
                 onChange={(date) => setEndDate(date)}
-                className="w-full border-2 border-vert rounded-xl px-2 py-1"
-                placeholderText="Choisissez une date de fin"
+                className="w-full border-2 border-vert rounded-xl px-1 py-px lg:px-2 lg:py-1"
+                placeholderText="02/01/1970"
                 locale="fr"
                 dateFormat="dd/MM/yyyy"
                 startDate={startDate || undefined}
@@ -61,7 +61,7 @@ const ModalGardien: FC<ModalGardienProps> = ({ isOpen, onClose, plant, children 
               />
             </div>
           </div>
-          <button name="gardiennage" type="submit" className="w-full bg-vert rounded-xl font-semibold py-1.5 text-white">Envoyer</button>
+          <button name="gardiennage" type="submit" className="w-full text-sm lg:text-base bg-vert rounded-xl font-semibold py-1 lg:py-1.5 text-white">Envoyer</button>
         </form>
         {children}
       </div>
